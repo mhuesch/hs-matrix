69f1c3e147f84af7a6af0f996edf5531ef24b772
Michael Hueschen <m@mhueschen.space>
2023-01-18 05:59:33 -0700

    tentative data model

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
+
+import Lens.Micro.TH
+
+data Snake =
+  Snake
+    { _sBody :: [Char]
+    , _sHeadIdx :: Int
+    }
+makeLenses ''Snake
+
+data Column =
+  Column
+    { _cSnakes :: [Snake]
+    }
+makeLenses ''Column
 
 main :: IO ()
 main = putStrLn "hi 👋"
