b4d632e1504235fa1643232d8dbe9ea0cd47a856
Michael Hueschen <m@mhueschen.space>
2023-01-18 06:26:40 -0700

    crude progress w/o MonadRandom

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,9 @@
 module Main where
 
 import Graphics.Vty
+import Lens.Micro ((^.), (%~), (&))
 import Lens.Micro.TH
+import System.Random (RandomGen, randomR)
 
 
 data Snake =
@@ -22,11 +24,36 @@ main :: IO ()
 main = do
   cfg <- standardIOConfig
   vty <- mkVty cfg
-  let line0 = string (defAttr ` withForeColor ` red) "first line"
-      line1 = string (defAttr ` withBackColor ` blue) "second line"
-      img = line0 <-> line1
-      pic = picForImage img
-  update vty pic
-  e <- nextEvent vty
+  let initModel = [] :: [Column]
+  _ <- go initModel
   shutdown vty
-  print ("last event was: " ++ show e)
+  putStrLn "bye 👋"
+
+go :: [Column] -> IO ()
+go model = do
+  -- let line0 = string (defAttr ` withForeColor ` red) "first line"
+  --     line1 = string (defAttr ` withBackColor ` blue) "second line"
+  --     img = line0 <-> line1
+  --     pic = picForImage img
+  -- update vty pic
+  -- e <- nextEvent vty
+  pure ()
+
+stepColumn :: RandomGen g => g -> Column -> (Column, g)
+stepColumn gen c = (c, gen)
+
+stepSnake :: RandomGen g => g -> Snake -> (Snake, g)
+stepSnake gen s = ( s & sHeadIdx %~ succ
+                      & sBody %~ extendBody
+                  , gen')
+ where
+  (c, gen') = genSnakeChar gen
+  extendBody body = let len = length body
+                     in take len (c:body)
+
+-- | this exercises a decent chunk of the printable range
+genSnakeChar :: RandomGen g => g -> (Char, g)
+genSnakeChar = randomR ('!', '~')
+
+isSnakeOffscreen :: Int -> Snake -> Bool
+isSnakeOffscreen height s = height < ((s ^. sHeadIdx) - (length (s ^. sBody)))
