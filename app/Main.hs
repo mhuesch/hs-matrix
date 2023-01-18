36fd32da992e6ab200c4a5d4a2006e0228b9c0e5
Michael Hueschen <m@mhueschen.space>
2023-01-18 08:18:36 -0700

    mostly works, but is quite dense

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-import Control.Concurrent (threadDelay)
-import Control.Monad (replicateM)
-import Control.Monad.Random (MonadRandom, runRand)
-import Control.Monad.Random.Class (getRandomR)
-import Graphics.Vty
-import Lens.Micro ((^.), (%~), (&))
-import Lens.Micro.TH
-import System.Console.Terminal.Size (Window(..), size)
-import System.Random (StdGen, getStdGen)
+import           Control.Concurrent (threadDelay)
+import           Control.Monad (forM_, replicateM, when)
+import           Control.Monad.ST (runST)
+import           Control.Monad.Random (MonadRandom, runRand)
+import           Control.Monad.Random.Class (getRandomR)
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import           Graphics.Vty
+import           Lens.Micro ((^.), (%~), (&))
+import           Lens.Micro.TH
+import           System.Console.Terminal.Size (Window(..), size)
+import           System.Random (StdGen, getStdGen)
 
 
 data Snake =
@@ -43,16 +47,33 @@ main = do
 
 go :: Vty -> Int -> StdGen -> [Column] -> IO ()
 go vty height gen cs = do
-  threadDelay (10^(5::Int))
+  threadDelay (5 * 10^(4::Int))
   let (cs', gen') = runRand (mapM (stepColumn height) cs) gen
-  let line0 = string (defAttr `withForeColor` red) (show cs')
-      pic = picForImage line0
+      imgCols = map (renderColumn height) cs'
+      pic = picForImage (horizCat imgCols)
   update vty pic
   mE <- nextEventNonblocking vty
   case mE of
     Just (EvKey KEsc _) -> pure ()
     Just (EvKey (KChar 'q') _) -> pure ()
+    Just (EvKey (KChar 'c') [MCtrl]) -> pure ()
     _ -> go vty height gen' cs'
+
+renderColumn :: Int -> Column -> Image
+renderColumn height col = vertCat (V.toList vec)
+ where
+  vec :: Vector Image
+  vec = runST $ do
+    v <- MV.replicate height (char defAttr ' ')
+    forM_ (col^.cSnakes) $ \snake -> do
+      forM_ (zip [0..] (snake^.sBody)) $ \(offset, c) -> do
+        let attr = if offset == 0
+                      then (defAttr `withForeColor` white)
+                      else (defAttr `withForeColor` green)
+            idx = snake^.sHeadIdx - offset
+        when (0 <= idx && idx < height) $
+          MV.write v idx (char attr c)
+    V.unsafeFreeze v
 
 stepColumn
   :: MonadRandom m
@@ -61,7 +82,7 @@ stepColumn
   -> m Column
 stepColumn height c = do
   snakesStepped <- mapM stepSnake (c ^. cSnakes)
-  let snakesFiltered = filter (isSnakeOffscreen height) snakesStepped
+  let snakesFiltered = filter (not . isSnakeOffscreen height) snakesStepped
   Column <$> spawnSnakes snakesFiltered
 
 stepSnake :: MonadRandom m => Snake -> m Snake
@@ -79,7 +100,7 @@ genSnakeChar = getRandomR ('!', '~')
 
 spawnSnakes :: MonadRandom m => [Snake] -> m [Snake]
 spawnSnakes [] = (:[]) <$> spawnSnake
-spawnSnakes (s:rest) | (s^.sHeadIdx > 5) = (:s:rest) <$> spawnSnake
+spawnSnakes (s:rest) | (snakeTailIdx s > 5) = (:s:rest) <$> spawnSnake
 spawnSnakes (s:rest) = pure (s:rest)
 
 spawnSnake :: MonadRandom m => m Snake
@@ -89,5 +110,8 @@ spawnSnake = do
   body <- replicateM len genSnakeChar
   pure (Snake body headIdx)
 
+snakeTailIdx :: Snake -> Int
+snakeTailIdx s = (s ^. sHeadIdx) - (length (s ^. sBody))
+
 isSnakeOffscreen :: Int -> Snake -> Bool
-isSnakeOffscreen height s = height < ((s ^. sHeadIdx) - (length (s ^. sBody)))
+isSnakeOffscreen height s = snakeTailIdx s >= height
